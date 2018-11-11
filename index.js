6d0b438aa569761b8182da2cc06756c41da6c418
aranajhonny <jhonnyjosearana@gmail.com>
2018-11-11 11:48:12 -0430

    fix index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const axios = require("axios");
 
 module.exports = async (req, res) => {
   try {
-    const data = await axios.get("https://github.com");
-    if (data) {
-      res.end("ok");
+    const result = await axios.get("https://api.github.com/");
+    if (result) {
+      res.end("ok " + result.status);
     }
   } catch (e) {
     console.log(e)
